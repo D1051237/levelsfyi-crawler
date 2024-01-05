8df70875220f580f5b311e16850122e0440f4150
9AY <92072836+D1051237@users.noreply.github.com>
2024-01-05 23:46:19 +0800

    Update test_levels_fyi_crawler.py

--- tests/test_levels_fyi_crawler.py
+++ tests/test_levels_fyi_crawler.py
@@ -1,13 +1,13 @@
 import unittest
 from unittest.mock import patch
-import levels_fyi_crawler 
+from levelsfyi_crawler import main_function
 
 class TestLevelsFYICrawler(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['d', 'max_tc', 'exit'])
-    def test_levels_fyi_crawler_with_mock_inputs(self, mock_input):
+    def test_main_function(self, mock_input):
 
-        result = levels_fyi_crawler.main_function() 
+        result = main_function()
 
         self.assertEqual(result, expected_result)
 
